f5bad1eafe553a8e206c6721c4b0c5f8a9d9454c
Saydum <saydum.it5@gmail.com>
2024-09-14 13:43:41 +0300

    feat: set CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import models
 from routes import router
 from config import engine
@@ -10,5 +11,13 @@ from config import engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # URL вашего Nuxt.js фронтенда
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все методы (GET, POST, DELETE и т.д.)
+    allow_headers=["*"],  # Разрешить все заголовки
+)
 
 app.include_router(router, tags=["employee"])
