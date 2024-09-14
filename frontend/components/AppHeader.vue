07955aed2bda9f922eee834252483945f952e8f1
Saydum <saydum.it5@gmail.com>
2024-09-15 02:22:00 +0300

    feat: create header vue component

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -1,26 +1,25 @@
 <template>
-    <div class="d-flex">
-      <div class="pb-4 flex-grow-1">
-        <h3>{{ title }}</h3>
-      </div>
-      <div v-if="showButton" class="p-2">
-        <!-- @TODO added link -->
-        <a href="#" class="btn btn-outline-success">{{ buttonText }}</a>
-      </div>
+  <div class="d-flex">
+    <div class="pb-4 flex-grow-1">
+      <h3>{{ title }}</h3>
     </div>
-  </template>
-  
-  <script lang="ts" setup>
-  import { defineProps } from 'vue';
-  
-  // Определяем свойства компонента
-  const props = defineProps<{
-    title: string;
-    showButton?: boolean;
-    buttonText?: string;
-  }>();
-  
-  // Значения по умолчанию для свойств
-  props.showButton = props.showButton ?? true;
-  props.buttonText = props.buttonText ?? 'New Employee';
-  </script>
+    <div v-if="showButton" class="p-2">
+      <a href="/add" class="btn btn-outline-success">{{ buttonText }}</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+// Определяем свойства компонента
+const props = defineProps<{
+  title: string;
+  showButton?: boolean;
+  buttonText?: string;
+}>();
+
+// Значения по умолчанию для свойств
+props.showButton = props.showButton ?? true;
+props.buttonText = props.buttonText ?? 'New Employee';
+</script>
