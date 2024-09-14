1cfe51fc73c3e28848a7e7bec40f33d6b5b63454
Saydum <saydum.it5@gmail.com>
2024-09-15 02:21:20 +0300

    feat: set dotenv

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,8 +1,13 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost/postgres"
+load_dotenv()
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
